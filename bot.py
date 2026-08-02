9ee5f1136297b78f307218def1075d017d44abe5
manojraihp555-netizen <manojraihp555@gmail.com>
2026-08-02 17:35:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,22 @@
 import logging
-from telegram.ext import ApplicationBuilder, CommandHandler
+
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+)
+
 from config import TOKEN
 from database import init_db
+
 from handlers import (
     start,
     help_command,
     myid,
     add_note_conv_handler,
     delete_note_conv_handler,
+    note_conv_handler,
+    edit_note_conv_handler,
+    allnotes,
 )
 
 logging.basicConfig(
@@ -23,14 +32,22 @@ def main():
 
     application = ApplicationBuilder().token(TOKEN).build()
 
+    # Basic Commands
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("myid", myid))
 
+    # Student Commands
+    application.add_handler(CommandHandler("allnotes", allnotes))
+    application.add_handler(note_conv_handler)
+
+    # Admin Commands
     application.add_handler(add_note_conv_handler)
     application.add_handler(delete_note_conv_handler)
+    application.add_handler(edit_note_conv_handler)
 
-    logger.info("Vishesh Study Bot Started...")
+    logger.info("✅ Vishesh Study Bot Started Successfully...")
+
     application.run_polling()
 
 
