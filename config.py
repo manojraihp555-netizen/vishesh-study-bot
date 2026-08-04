8c96df7710627de812fbda3878da8aace27b3ecc
manojraihp555-netizen <manojraihp555@gmail.com>
2026-08-04 15:28:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,17 @@
 import os
-from import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Telegram Bot Token
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+BOT_TOKEN = os.getenv("TOKEN")
 
 # Database
 DATABASE_NAME = "study_bot.db"
 
-# Admin IDs (Multiple admins supported)
+# Admin IDs
 ADMIN_IDS = [
-    int(admin_id)
-    for admin_id in os.getenv("ADMIN_IDS", "").split(",")
-    if admin_id.strip()
-] import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-ADMIN_IDS = [
-    int(i) for i in os.getenv("ADMIN_IDS", "").split(",") if i.strip()
+    int(i)
+    for i in os.getenv("ADMIN_IDS", "").split(",")
+    if i.strip()
 ]
-
-DB_FILE = "study_bot.db"
